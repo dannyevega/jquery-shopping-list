6c9958dc385a05606a64377a231246aee7ac9eec
Danny Vega <dannyevega@gmail.com>
2015-10-04 21:11:06 -0700

    add prevent default function. moved script tags to bottom of page so no need for document.ready function. changed event listener to .submit and created addItem function. check-off toggle class works now

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
-$(document).ready(function(){
-	//create variables for the 'minus' remove button and 'check' complete button
-	var removeButton = '<i id="remove" class="fa fa-minus-circle"></i>';
-	var checkMark = '<i id="checkmark" class="fa fa-check-circle"></i>';	
+//create variables for the 'minus' remove button and 'check' complete button
+var removeButton = '<i id="remove" class="fa fa-minus-circle"></i>';
+var checkMark = '<i id="checkmark" class="fa fa-check-circle"></i>';	
 
-	//allows users to add items using the 'enter' button
-	$('#item-input').keyup(function(e){
-		if(e.which == 13){
-			e.preventDefault();
-			$('#add-button').click();
-		}
-	});
+//allows users to add items using the 'ADD' button
+$('.item-container .new-item').submit(instead(addItem));
 
-	//allows users to add items using the 'ADD' button
-	$('#add-button').click(function(){
-		if($('#item-input').val().trim().length === 0){
-			$('.error-message').show();
-			$('#item-input').val('');
-		} else {
-			$('.error-message').hide();
-			var itemContent = $('#item-input').val();
-			var appendItem = '<li id="draggable">' + removeButton + itemContent + checkMark + '</li>'
-			$('.list-area').append(appendItem);
-			$('#item-input').val('');				
-		}
-	});
-
-	removeItem();		
-	checkOff();
-	emptyList();
-
-});
+removeItem();		
+checkOff();
+emptyList();
 
 //allows you to sort the items in the list
-$(function() {
-  $( "#sortable" ).sortable();
-  $( "#sortable" ).disableSelection();
-});
+$( "#sortable" ).sortable();
+$( "#sortable" ).disableSelection();
+
+function instead(fn){
+	return function(e){
+		e.preventDefault();
+		fn.apply(this);
+	};
+}
+
+function addItem(event){
+	// event.preventDefault();
+	if($('#item-input').val().trim().length === 0){
+		$('.error-message').show();
+		$('#item-input').val('');
+	} else {
+		$('.error-message').hide();
+		var itemContent = $('#item-input').val();
+		// var appendItem = '<li>' + removeButton + itemContent + checkMark + '</li>'
+		var listItem = $('<li>').text(itemContent).prepend(removeButton).append(checkMark);		
+		$('.list-area').append(listItem);
+		$('#item-input').val('');				
+	}
+}
 
 //allows user to remove an item from the list
 function removeItem(){
@@ -43,15 +41,11 @@ function removeItem(){
 		$(this).closest('li').slideUp("slow");
 	});	
 }
-//how come the above will work but when I call it this way inside the first $(document).ready
-// $('#remove').click(function(){
-// 	$(this).closest('li').slideUp("slow");
-// })
 
 //allows user to check an item off and uncheck it as well
 function checkOff(){
 	$(document).on("click", "#checkmark", function(){
-		$(this).closest('li').toggleClass("check-off").css("color", "white");
+		$(this).closest('li').toggleClass("check-off");
 	});	
 }
 
@@ -61,3 +55,10 @@ function emptyList(){
 		$('.list-area').empty();
 	});	
 }
+
+//how come the above will work but when I call it this way inside the first $(document).ready
+//this wont work because when the page loads, there is no li items with id="remove"
+//look up jquery event delegation
+// $('#remove').click(function(){
+// 	$(this).closest('li').slideUp("slow");
+// })
